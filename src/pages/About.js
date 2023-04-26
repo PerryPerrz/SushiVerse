9f170632081feb573fff611e98a95ff910164560
PerryPerrz <iophugstud6@gmail.com>
2023-04-26 22:19:52 +0200

    Data et CSS du About Us

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,9 @@ function About() {
             <div className='aboutTop' style={{ backgroundImage: `url(${BannerImage})` }}></div>
             <div className='aboutBottom'>
                 <h1 className='aboutTitle'>About Us</h1>
-                <p>Sushi Gods</p>
+                <p>"SushiVerse" est né de la passion de son fondateur, le chef sushi Hiro, pour la cuisine japonaise.</p>
+                <p>Son rêve était de créer un magasin de sushi unique en son genre, où les clients pourraient découvrir des sushis innovants et surprenants, préparés avec les ingrédients les plus frais et les techniques les plus fines.</p>
+                <p>Aujourd'hui, SushiVerse est devenu une destination incontournable pour tous ceux qui cherchent une expérience culinaire unique, dans un cadre chaleureux et accueillant.</p>
             </div>
         </div>
     )
