09cb2855eb2a25a3130e3e48a84f79475ba237d6
PerryPerrz <iophugstud6@gmail.com>
2023-05-20 18:31:22 +0200

    Upgrade the contact's form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import BannerImage from '../assets/ramen.jpeg'
+import { useForm, ValidationError } from '@formspree/react';
 
 import '../styles/Contact.css'
 
 function Contact() {
+    const [state, handleSubmit] = useForm("mdovjqrd");
+
     return (
         <div
             className='contact'>
@@ -11,17 +14,54 @@ function Contact() {
             <div className='rightSide'>
                 <h1> Contact Us</h1>
 
-                <form id="contact-form" method="POST">
+                <form id="contact-form" method="POST" onSubmit={handleSubmit}>
                     <label htmlFor="name">Full Name</label>
-                    <input name="name" placeholder="Enter full name..." type="text" />
+                    <input
+                        id="name"
+                        placeholder="Enter full name..."
+                        name="name"
+                        type="text"
+                        pattern='[A-Za-z\s]+'
+                        required
+                    />
+                    <ValidationError
+                        prefix="Name"
+                        field="name"
+                        errors={state.errors}
+                    />
 
                     <label htmlFor="email">Email</label>
-                    <input name="email" placeholder="Enter email..." type="email" />
+                    <input
+                        id="email"
+                        placeholder="Enter email..."
+                        name="email"
+                        type="email"
+                        pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+                        required
+                    />
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
 
                     <label htmlFor="message">Message</label>
-                    <textarea rows="6" name="message" placeholder="Enter message..." type="text" required />
+                    <textarea
+                        id="message"
+                        placeholder="Enter message..."
+                        rows="6"
+                        name="message"
+                        type="text"
+                        pattern='[A-Za-z\s]+'
+                        required
+                    />
+                    <ValidationError
+                        prefix="Message"
+                        field="message"
+                        errors={state.errors}
+                    />
 
-                    <button type="submit">Submit</button>
+                    <button type="submit" disabled={state.submitting || state.succeeded}><span>{!state.succeeded && !state.submitting ? "Submit" : state.succeeded ? "Submitted !" : "Submit in progress..."}</span></button>
                 </form>
             </div>
         </div>
